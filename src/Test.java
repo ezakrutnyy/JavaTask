a5ee3768a86934530abc4befbc5aa0059163b9c0
ezakrutnyy <zakrutnyi@gmail.com>
2025-05-28 12:12:17 +0300

    add articles of habr

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,15 @@
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
 public class Test {
-    public static void main(String[] args) {
-        String key = "key";
-        int h = 101603;
-        System.out.println();
-        System.out.println("index = " + (h & (16 - 1)));
+    public static void main(String[] args) throws IOException {
+        ZonedDateTime zonedDateTime = Instant.now().atOffset(ZoneOffset.of("-04:00")).toZonedDateTime();
+        System.out.println(zonedDateTime);
+        System.out.println(zonedDateTime.toLocalDate());
+        System.out.println(zonedDateTime.toEpochSecond());
+        System.out.println(new Date(zonedDateTime.toEpochSecond()));
     }
 }
